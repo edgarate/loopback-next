1968b0edc7d0d1e3a96d13304ea5ff17532bfe36
Nora <nora.abdelgadir@ibm.com>
2019-04-26 11:16:55 -0400

    fix: change from mysql to memory db

--- examples/lb3application/lb3app/server/boot/create-sample-models.js
+++ examples/lb3application/lb3app/server/boot/create-sample-models.js
@@ -1,22 +1,29 @@
 'use strict';
 
 module.exports = function(app) {
-  app.dataSources.mysqlDs.automigrate('CoffeeShop', function(err) {
+  app.dataSources.db.automigrate('CoffeeShop', function(err) {
     if (err) throw err;
 
-    app.models.CoffeeShop.create([{
-      name: 'Bel Cafe',
-      city: 'Vancouver',
-    }, {
-      name: 'Three Bees Coffee House',
-      city: 'San Mateo',
-    }, {
-      name: 'Caffe Artigiano',
-      city: 'Vancouver',
-    }], function(err, coffeeShops) {
-      if (err) throw err;
+    app.models.CoffeeShop.create(
+      [
+        {
+          name: 'Bel Cafe',
+          city: 'Vancouver',
+        },
+        {
+          name: 'Three Bees Coffee House',
+          city: 'San Mateo',
+        },
+        {
+          name: 'Caffe Artigiano',
+          city: 'Vancouver',
+        },
+      ],
+      function(err, coffeeShops) {
+        if (err) throw err;
 
-      console.log('Models created: \n', coffeeShops);
-    });
+        console.log('Models created: \n', coffeeShops);
+      },
+    );
   });
 };
